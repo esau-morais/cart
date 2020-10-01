cb95988f8b8bf759edb0d558d8eebb1c32544b31
esau-morais <esaumorais7@gmail.com>
2020-10-01 18:08:10 -0300

    feat: shift to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Components
+import NavBar from './components/NavBar';
+import Landing from './components';
+import SignLog from './components/SignLog';
+import PageNotFound from './components/404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/signlog" component={SignLog} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
+    </Router>
   );
 }
 
